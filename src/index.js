232edccce62ea3869ae657869151d49fad263576
MRHM22 <hdezmartin22@gmail.com>
2023-10-26 14:15:34 -0600

    cambio

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import ComidaApp from './ComidaApp';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ComidaApp />
   </React.StrictMode>
 );
 
