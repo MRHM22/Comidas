8e062897cf6898f8cbaed98b107d6371add83219
MRHM22 <hdezmartin22@gmail.com>
2023-10-26 16:09:14 -0600

    reducer

--- src/store/comidasSlice.js
+++ src/store/comidasSlice.js
@@ -1,6 +1,22 @@
 
 import { createSlice  }  from '@reduxjs/toolkit';
 
-export const comidasSlice = {
+export const comidasSlice = createSlice({
+    name: 'comidas',
+    initialState: {
+        foods: [
+           // tempEvent
+        ],
+        activeFood: null
+    },
+    reducers: {
+        onLoadFood: (state, {payload}) => {
+            payload.forEach(element => {
+                state.foods.push(element);
+            });
+        }
+    }
     //https://www.themealdb.com/
-}
+});
+
+export const { onLoadFood } = comidasSlice.actions;
